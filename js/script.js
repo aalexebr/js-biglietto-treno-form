4a7dae964be04005e2183be40a0bb2a856f721a9
aalexebr <alex.ebr@icloud.com>
2023-06-13 19:49:11 +0200

    fixing value

--- js/script.js
+++ js/script.js
@@ -6,28 +6,33 @@ generate.addEventListener("click",
             alert('idiot')
         }
         else{
-            const select = document.getElementById('age');
-            const value = select.options[select.selectedIndex].value;
+            const select = document.getElementById('age').value; 
+            // remove .value from above for below to work
+            // const value = select.options[select.selectedIndex].value;
             const minor = document.getElementById('minor').value
             const over = document.getElementById('over').value
+          
             let price = km * 0.21
             document.getElementById("discountType").innerHTML = "Prezzo standard"
-            if(value == minor){
+           
+            // condition in a secect if previous comment on select below must be value instead
+            if(select == minor){
                 price -= price * 0.2
                 document.getElementById("discountType").innerHTML = "Prezzo ridotto"
             }
-            else if(value == over){
+            else if(select == over){
                 price -= price * 0.4
                 document.getElementById("discountType").innerHTML = "Prezzo over65"
             }
-            console.log('final', price )
+            
             document.getElementById('cost').innerHTML = price.toFixed(2) + '$'
             
-        
+            // adding the value from input and putting it in html
             const name = document.getElementById("name").value
             document.getElementById("passName").innerHTML = name
 
-            let carrigeNumber = Math.floor(Math.random()*(10 - 1 +1) + 1)
+            // adding random number generator
+            let carrigeNumber = Math.floor(Math.random()*(10 - 1 + 1) + 1)
             document.getElementById("carrige").innerHTML = carrigeNumber
 
             let ticketCode = Math.floor(Math.random()*(9999 - 1000 +1) + 1000)
