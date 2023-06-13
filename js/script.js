52ac314822bed570351615192a60fbaa3b69184a
aalexebr <alex.ebr@icloud.com>
2023-06-13 14:32:21 +0200

    basic function

--- js/script.js
+++ js/script.js
@@ -1,7 +1,28 @@
-const generate = document.getElementById('submit')
+const generate = document.getElementById("submit")
 generate.addEventListener("click",
     function(){
+        const km = parseFloat(document.getElementById("km").value);
+        const select = document.getElementById('age');
+        const value = select.options[select.selectedIndex].value;
+        const minor = document.getElementById('minor').value
+        const over = document.getElementById('over').value
+        let price = km * 0.21
+        if(value == minor){
+            price -= price * 0.2
+            console.log('minor', price )
+        }
+        else if(value == over){
+            price -= price * 0.4
+            console.log('over', price )
+        }
+        console.log('none', price )
         
+       
+    
+
+        
+
+
     }
     
 );
